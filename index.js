aea5d6108343ad7da19c110289cc6e6f2ab72214
inesssik <denya.savchenko@gmail.com>
2022-06-07 03:15:43 +0300

    Commit

--- index.js
+++ index.js
@@ -55,3 +55,19 @@ $button.addEventListener('click', () => {
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
+
+const myInput = document.getElementById("countOfPlayers");
+function stepper(btn){
+    let id = btn.getAttribute("id");
+    let min = myInput.getAttribute("min");
+    let max = myInput.getAttribute("max");
+    let step = myInput.getAttribute("step");
+    let val = myInput.getAttribute("value");
+    let calcStep = (id == "increment") ? (step*1) : (step * -1);
+    let newValue = parseInt(val) + calcStep;
+
+    if(newValue >= min && newValue <= max){
+        myInput.setAttribute("value", newValue);
+    }
+}
